47d53b685ee72bed4763d33ec29bed030717b0b8
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2021-09-21 16:50:31 +0300

    +++

--- LB__10_past_course/LB__10G/Main.cpp
+++ LB__10_past_course/LB__10G/Main.cpp
@@ -1,1 +1,20 @@
 #include "Timer.h"
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+	TIMER Second(9);                //Если объект принимает 1 аргумент, значит работа с секундами
+	Second.show();
+
+	TIMER Minute_second(3, 41);    //Если объект принимает 2 аргумента, значит работа с минутами и сек.
+	Minute_second.show();
+
+	TIMER Copy(Second);         //Копируем один из оъектов
+	Copy.show();
+
+
+
+}
+
--- LB__10_past_course/LB__10G/Timer.h
+++ LB__10_past_course/LB__10G/Timer.h
@@ -7,8 +7,22 @@
 using namespace std;
 
 class TIMER {
-	int time_sc;
-	int time_sc_
+	int second;           //Переменная для секунд
+	int minute = 0;      //Переменная для минут
 
+public:
+	TIMER(int s) :second(s) {}                          //Конструктор с параметрами, для инициализации секунд
+	TIMER(int m, int s) :minute(m), second(s) {}        //Конструктор с параметрами, для инициализации минут и секунд
+	TIMER(const  TIMER& ob) : minute(ob.minute), second(ob.second) {}    //Конструктор копирования
 
+	void show() {                           //В зависимости от того что приймет конструктор, то и будет выведенно
+		if (minute == 0)
+		{
+			cout << "Функция вывода кол-ва секунд -> " << second << endl;
+		}
+		else
+		{
+			cout << "Функция вывода кол-ва минут и секунд -> " << minute << ":" << second << endl;
+		}
+	}
 };
